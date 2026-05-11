b8df51a8b9f93b6d9aa472075972e7ce2aa9b7e6
cjordaoc <139711623+cjordaoc@users.noreply.github.com>
2026-05-11 18:37:43 -0300

    Update GRC.Shared.UI to use Microsoft.NET.ILLink.Tasks version 8.0.25 and adjust related dependencies
    - Updated Microsoft.NET.ILLink.Tasks from version 8.0.24 to 8.0.25 in project files and dependencies.
    - Added restore audit properties and SDK analysis level to project configuration.
    - Adjusted runtime identifier graph path for compatibility.
    - Updated NuGet tool version from 6.8.1 to 7.0.0.
    - Modified project.assets.json to reflect new package versions and dependencies.
    - Updated project.nuget.cache to ensure consistency with new package versions.

--- GRC.Shared.Resources/obj/Debug/net8.0/GRC.Shared.Resources.AssemblyInfo.cs
+++ GRC.Shared.Resources/obj/Debug/net8.0/GRC.Shared.Resources.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GRC.Shared.Resources")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51dd3f315adda9f1fefb36734841761e6497ed82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b17250d63406e513cae2e8f88383748ab46b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("GRC.Shared.Resources")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GRC.Shared.Resources")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
